a1bf33cb09de315fa3a6592c064b41314027b9cc
andrewbatallones <batallonesa@gmail.com>
2023-07-24 16:34:04 -0400

    can handle template loading

--- server.go
+++ server.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello")
+	t, _ := template.ParseFiles("app/views/index.html")
+	t.Execute(w, "")
 }
 
 func main() {
+	fmt.Println("server is up...")
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
